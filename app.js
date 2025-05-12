cb78b6f33c9d5f30b0743e1649164d2a375fc8ee
MradulGandhi <gandhisir80@gmail.com>
2025-05-12 11:45:52 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const { Chess } = require('chess.js');
+const http = require('http');
+const socket = require('socket.io');
+const path = require('path');
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
+let noofplayers = 0;
+let players = {};
+let currentPlayer = 'w';
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Chess Game' });
+});
+
+io.on("connection", (uniquesocket) => {
+    console.log("connected");
+    noofplayers++;
+    console.log("Number of players: " + noofplayers);
+    if (noofplayers >= 2) {
+       console.log("Game started");
+        io.emit("gameStarted");
+       
+    }
+
+   
+    uniquesocket.on("disconnect", () => {
+        if (uniquesocket.id === players.white) {
+            delete players.white;
+        }
+        if (uniquesocket.id === players.black) {
+            delete players.black;
+        }
+        noofplayers--;
+        console.log("disconnected");
+    });
+
+    if (!players.white) {
+        players.white = uniquesocket.id;
+        uniquesocket.emit("playerRole", "w");
+    } else if (!players.black) {
+        players.black = uniquesocket.id;
+        uniquesocket.emit("playerRole", "b");
+    } else {
+        uniquesocket.emit("playerRole", "spectator");
+    }
+
+    uniquesocket.on("move", (move) => {
+        try {
+            if (chess.turn() === 'w' && uniquesocket.id !== players.white) return;
+            if (chess.turn() === 'b' && uniquesocket.id !== players.black) return;
+           
+            const result = chess.move(move);
+            if (result) {
+                currentPlayer = chess.turn();
+                io.emit("move", move);
+                io.emit("boardState", chess.fen());
+            }
+            else {
+                console.log("Invalid move");
+                uniquesocket.emit("invalidMove", move);
+            }
+            io.emit("history", move)
+           
+        }
+        catch (err) {
+            
+
+            uniquesocket.emit("invalidMove", move);
+        }
+
+    });
+
+
+
+});
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
---
+++ public/js/chessgame.js
@@ -0,0 +1,209 @@
+const socket = io();
+const chess = new Chess();
+const statusBar = document.querySelector(".statusBar");
+const wHistory = document.querySelector(".whitehistory");
+const bHistory = document.querySelector(".blackhistory");
+const boardElement = document.querySelector(".chessboard");
+const roleElement = document.querySelector(".role");
+let draggedPiece = null;
+let squareSource = null;
+let playerRole = null;
+
+
+// Music items
+const startMusic = new Audio("/music/startMusic.mp3");
+const moveMusic = new Audio("/music/move.mp3");
+const invalidMoveMusic = new Audio("/music/invalidmove.mp3");
+// const gameOverMusic = new Audio("/audio/gameover.mp3");
+
+function playMusic(e) {
+    e.play();
+}
+
+function stopMusic(e) {
+    e.pause();
+    e.currentTime = 0;
+}
+
+const renderBoard = () => {
+    const board = chess.board();
+    boardElement.innerHTML = "";
+
+
+
+
+    board.forEach((row, rowIndex) => {
+        const rowElement = document.createElement("div");
+        rowElement.classList.add("row");
+
+        row.forEach((square, squareIndex) => {
+            const squareElement = document.createElement("div");
+            squareElement.classList.add("square", (rowIndex + squareIndex) % 2 === 0 ? "light" : "dark");
+            squareElement.dataset.row = rowIndex;
+            squareElement.dataset.col = squareIndex;
+
+            if (square) {
+                const pieceElement = document.createElement("div");
+                pieceElement.classList.add("piece", square.color === 'w' ? "white" : "black");
+                pieceElement.innerHTML = getPieceUnicode(square);
+                pieceElement.draggable = playerRole === square.color;
+
+                pieceElement.addEventListener("dragstart", (e) => {
+                    if (pieceElement.draggable) {
+                        draggedPiece = pieceElement;
+                        squareSource = { row: rowIndex, col: squareIndex, type: pieceElement.innerHTML };
+
+                        e.dataTransfer.setData("text/plain", "");
+                    }
+
+                })
+                pieceElement.addEventListener("dragend", (e) => {
+                    e.preventDefault();
+                    draggedPiece = null;
+                    squareSource = null;
+                });
+                squareElement.appendChild(pieceElement);
+            }
+            // squareElement.innerHTML= "."
+
+
+
+
+            squareElement.addEventListener("dragover", (e) => {
+                e.preventDefault();
+
+            });
+            squareElement.addEventListener("drop", (e) => {
+                e.preventDefault();
+                if (draggedPiece) {
+                    const targetSource = {
+                        row: parseInt(squareElement.dataset.row),
+                        col: parseInt(squareElement.dataset.col)
+                    }
+
+                    handleMove(squareSource, targetSource);
+                }
+
+            }
+            );
+            // squareElement.addEventListener("click", handleClick);
+            // squareElement.appendChild(pieceElement)
+
+            rowElement.appendChild(squareElement);
+        });
+
+        boardElement.appendChild(rowElement);
+    });
+
+    //flipping
+
+    if (playerRole === 'b') {
+        boardElement.classList.add("flipped");
+    }
+
+}
+
+
+
+const handleMove = (source, target) => {
+    const move = {
+        from: `${String.fromCharCode(97 + source.col)}${8 - source.row}`,
+        to: `${String.fromCharCode(97 + target.col)}${8 - target.row}`,
+        promotion: 'q',
+        color: playerRole,
+        type: draggedPiece.innerHTML
+    }
+
+    socket.emit("move", move);
+
+
+
+}
+const getPieceUnicode = (piece) => {
+    const unicodePieces = {
+        p: "♙",
+        r: "♖",
+        n: "♘",
+        b: "♗",
+        q: "♕",
+        k: "♔",
+        P: "♟",
+        R: "♜",
+        N: "♞",
+        B: "♝",
+        Q: "♛",
+        K: "♚",
+    }
+    return unicodePieces[piece.type] || "";
+}
+
+socket.on("playerRole", (role) => {
+    playerRole = role;
+    renderBoard();
+
+    if (role === "w") {
+        roleElement.innerHTML = "You are playing as White";
+    } else if (role === "b") {
+        roleElement.innerHTML = "You are playing as Black";
+    }
+    else {
+        roleElement.innerHTML = "You are a spectator ! You can only watch the game";
+    }
+
+});
+
+socket.on("spectator", (params) => {
+    playerRole = null;
+    renderBoard();
+
+});
+
+socket.on("boardstate", function (fen) {
+    chess.load(fen);
+    renderBoard();
+})
+
+
+socket.on("move", (move) => {
+    chess.move(move);
+    renderBoard();
+
+}
+);
+socket.on("gameStarted", () => {
+    // alert("Game Started");
+    statusBar.innerHTML = "Game Started.....";
+    // playMusic(startMusic);
+
+    renderBoard();
+
+});
+
+// invalidmove
+socket.on("invalidMove", (move) => {
+    playMusic(invalidMoveMusic);
+    console.log("Invalid move", move);
+    statusBar.innerHTML = "Invalid Move";
+});
+
+socket.on("history", (move) => {
+
+    playMusic(moveMusic);
+
+    if (move.color === 'w') {
+        const historyElement = document.createElement("div");
+        historyElement.classList.add("history");
+        historyElement.innerHTML = `${move.type} ${move.from} to ${move.to}`;
+        wHistory.insertBefore(historyElement, wHistory.firstChild);
+    }
+    else {
+        const historyElement = document.createElement("div");
+        historyElement.classList.add("history");
+        historyElement.innerHTML = `${move.type} ${move.from} to ${move.to}`;
+        bHistory.insertBefore(historyElement, bHistory.firstChild);
+
+
+    }
+})
+
+// renderBoard()
